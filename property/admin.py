44dc7927a36b342432e0ffed2208f6ebb50ff3cd
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-15 21:46:40 +0300

    Add files via upload

---
+++ property/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+from .models import Flat
+from .models import Complaint
+from .models import Owner
+
+
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ["owner"]
+
+
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('town','address')
+    readonly_fields = ["created_at"]
+    list_display = ["address", "price", "new_building", "construction_year", "town"]
+    list_editable = ["new_building"]
+    list_filter = ["new_building", "rooms_number", "has_balcony"]
+    raw_id_fields = ["liked_by"]
+    inlines = [
+        OwnerInline,
+    ]
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ["user", "flat"]
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ["flat"]
+
+
+admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Owner, OwnerAdmin)
