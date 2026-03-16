c50a877bd4d0e08d6e7f1dc16d717ab8ee9afe18
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-16 14:52:55 +0300

    Update models.py changed the field name

--- property/models.py
+++ property/models.py
@@ -85,8 +85,8 @@ class Complaint(models.Model):
 
 class Owner(models.Model):
     name = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, db_index=True)
+    phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
+    pure_phonenumber = PhoneNumberField('Нормализованный номер владельца', blank=True, db_index=True)
     flat = models.ManyToManyField(
         Flat,
         verbose_name='Квартиры в собственности',
